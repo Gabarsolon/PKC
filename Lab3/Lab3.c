7f3a156387843209465a722454241ed2b54fd876
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-12-12 21:15:55 +0200

    LAB-3: Added BIG_NUMBERS ooooh yeah

--- Lab3/Lab3.c
+++ Lab3/Lab3.c
@@ -5,11 +5,13 @@
 #define false 0
 #define true 1
 
+typedef unsigned long long int BIG_NUMBER;
+
 // Utility function to do modular exponentiation.
 // It returns (x^y) % p
-int modularExponentiation(int x, unsigned int y, int p)
+BIG_NUMBER modularExponentiation(BIG_NUMBER x, BIG_NUMBER y, BIG_NUMBER p)
 {
-	int res = 1;	 // Initialize result
+	BIG_NUMBER res = 1;	 // Initialize result
 	x = x % p; // Update x if it is more than or
 	// equal to p
 	while (y > 0)
@@ -25,19 +27,19 @@ int modularExponentiation(int x, unsigned int y, int p)
 	return res;
 }
 
-int miillerTest(int n, int a, int s, int t)
+int miillerTest(BIG_NUMBER n, BIG_NUMBER a, BIG_NUMBER s, BIG_NUMBER t)
 {
 	//check if a^t = 1
-	int x = modularExponentiation(a, pow(2, 0) * t, n);
-	printf("For j=0: %d\n", x);
+	BIG_NUMBER x = modularExponentiation(a, pow(2, 0) * t, n);
+	printf("For j=0: %I64u\n", x);
 	if (x == 1)
 		return true;
 
-	int prevX = x;
-	for (int j = 1; j <= s; j ++) {
+	BIG_NUMBER prevX = x;
+	for (BIG_NUMBER j = 1; j <= s; j ++) {
 		//check if a^(2^j)*t = 1
 		x = modularExponentiation(a, pow(2, j) * t, n);
-		printf("For j=%d: %d\n", j, x);
+		printf("For j=%I64u: %I64u\n", j, x);
 		if (x == 1 && prevX == n-1)
 			return true;
 		prevX = n - 1;
@@ -49,9 +51,11 @@ int miillerTest(int n, int a, int s, int t)
 // It returns false if n is composite and returns true if n
 // is probably prime. k is an input parameter that determines
 // accuracy level. Higher value of k indicates more accuracy.
-int isPrime(int n, int k)
+int isPrime(BIG_NUMBER n, BIG_NUMBER k)
 {
 	// Corner cases
+	if (n < 2)
+		return false;
 	if (n == 2 || n == 3 || n == 5)
 		return true;
 	if (n % 2 == 0)
@@ -59,29 +63,29 @@ int isPrime(int n, int k)
 
 
 	// Find s and t such that n - 1 = 2^s * t
-	int s = 0;
-	int t = n - 1;
+	BIG_NUMBER s = 0;
+	BIG_NUMBER t = n - 1;
 	while (t % 2 == 0) {
 		s += 1;
 		t = t / 2;
 	}
 
 	printf("---------------------------------\n");
-	printf("s = %d\n", s);
-	printf("t = %d\n", t);
+	printf("s = %I64u\n", s);
+	printf("t = %I64u\n", t);
 
 	// Iterate given number of 'k' times
 
-	int a = 2;
+	BIG_NUMBER a = 2;
 	printf("------------------------------------\n");
-	printf("Iteration k=%d for a=%d\n", 1, a);
+	printf("Iteration k=%I64u for a=%d\n", 1, a);
 	if (!miillerTest(n, a, s, t))
 		return false;
 	a = 3;
 
 	for (int i = 1; i < k; i++) {
 		printf("------------------------------------\n");
-		printf("Iteration k=%d for a=%d\n", i + 1, a);
+		printf("Iteration k=%I64u for a=%d\n", i + 1, a);
 		if (!miillerTest(n, a, s, t))
 			return false;
 		a += 2;
@@ -95,20 +99,24 @@ int isPrime(int n, int k)
 int main()
 {
 	int k = 3; // Number of iterations
-	int n;
+	BIG_NUMBER n;
 
 	//for (int i = 2; i < 100; i++)
 	//	if (isPrime(i, k))
 	//		printf("\n\n\n\n\%d\n\n\n\n", i);
 
-	printf("Input n: ");
-	scanf("%d", &n);
-	
+	while (true) {
+		printf("Input n: ");
+		scanf("%I64u", &n);
 
-	if (isPrime(n, k))
-		printf("%d IS PRIME WITH PROBABILITY: %f\n", n, 1 - 1 / pow(4, k));
-	else
-		printf("%d IS COMPOSITE\n", n);
+		printf("-----------------------------------------------\n");
+		if (isPrime(n, k))
+			printf("%I64u IS PRIME WITH PROBABILITY: %f\n", n, 1 - 1 / pow(4, k));
+		else
+			printf("%I64u IS COMPOSITE\n", n);
+		printf("------------------------------------------------\n");
+	}
+
 		
 	return 0;
 }
